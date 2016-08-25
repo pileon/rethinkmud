d30e2b65d015ce23fce309c8f749f41d6274ca08
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-08-25 15:55:03 +0200

    Added simple printing of version

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <autoconf.h>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "RethinkMUD version " << RETHINKMUD_VERSION << '\n';
     return 0;
 }
