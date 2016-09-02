6a99271bd57f337b2ad05e8c1db5a558380f5830
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-09-02 11:00:50 +0200

    Added librethinkdbxx as a library
    The library is added as a git submodule and built as a static library linked to the main application.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include "autoconf.h"
 #include <iostream>
+#include "../rethinkdbxx/rethinkdb.h"
+
+namespace R = RethinkDB;
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "RethinkMUD version " << RETHINKMUD_VERSION << '\n';
+
+    auto conn = R::connect();
+    if (conn)
+    {
+        std::cout << "Connected\n";
+    }
+    else
+    {
+        std::cerr << "Could not connect to server\n";
+    }
+
     return 0;
 }
