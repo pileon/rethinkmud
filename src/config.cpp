9dae168bbd096fc722dd29a27c4f9d12a29667c6
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-09-22 14:05:50 +0200

    Configuration file parsing

--- src/config.cpp
+++ src/config.cpp
@@ -7,6 +7,7 @@
 
 #include <boost/program_options.hpp>
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 
 namespace po = boost::program_options;
@@ -17,29 +18,43 @@ namespace rethinkmud
     {
         namespace
         {
-            po::options_description get_command_line_options(std::string const &argv0)
+            auto get_command_line_options(std::string const &argv0)
             {
                 std::string default_config_file = argv0 + ".cfg";
 
-                po::options_description general{"General options"};
-                general.add_options()
+                po::options_description options{"General options"};
+                options.add_options()
                     ("help,h", "show help message")
                     ("version,v", "show program version")
                     ("config,c", po::value<std::string>()->default_value(default_config_file), "configuration file");
 
-                po::options_description options;
-                return options.add(general);
+                return options;
+            }
+
+            auto get_config_options()
+            {
+                po::options_description config;
+                return config;
+            }
+
+            auto get_common_options()
+            {
+                po::options_description common{"Common options"};
+                common.add_options()
+                    ("port,p", po::value<int>()->default_value(4000), "main port number");
+
+                return common;
             }
         }
 
         void load(int argc, char* argv[])
         {
             auto options = get_command_line_options(argv[0]);
-            //auto config = get_config_options();
-            //auto common = get_common_options();
+            auto config = get_config_options();
+            auto common = get_common_options();
 
-            //options.add(common);
-            //config.add(common);
+            options.add(common);
+            config.add(common);
 
             po::variables_map vm;
 
@@ -70,15 +85,23 @@ namespace rethinkmud
 
             std::cout << "Configuration file = " << vm["config"].as<std::string>() << '\n';
 
-            //try
-            //{
-            //    po::store(po::parse_command_line(argc, argv, config), vm, true);
-            //}
-            //catch (po::error& e)
-            //{
-            //    std::clog << "Error: " << e.what() << '\n';
-            //    std::exit(1);
-            //}
+            std::ifstream config_file{vm["config"].as<std::string>()};
+            if (config_file)
+            {
+                try
+                {
+                    po::store(po::parse_config_file(config_file, config), vm, true);
+                }
+                catch (po::error& e)
+                {
+                    std::clog << "Error: " << e.what() << '\n';
+                    std::exit(1);
+                }
+            }
+
+            std::cout << "Main port number = " << vm["port"].as<int>() << '\n';
+
+            // TODO: Environment variables?
 
         }
     }
