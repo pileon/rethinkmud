5e5e55e0ec5dbf8d88e108e455ba5e7c09fd675f
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-09-22 13:39:57 +0200

    Basic command-line argument handling

--- src/config.cpp
+++ src/config.cpp
@@ -15,19 +15,50 @@ namespace rethinkmud
 {
     namespace config
     {
+        namespace
+        {
+            po::options_description get_command_line_options(std::string const &argv0)
+            {
+                std::string default_config_file = argv0 + ".cfg";
+
+                po::options_description general{"General options"};
+                general.add_options()
+                    ("help,h", "show help message")
+                    ("version,v", "show program version")
+                    ("config,c", po::value<std::string>()->default_value(default_config_file), "configuration file");
+
+                po::options_description options;
+                return options.add(general);
+            }
+        }
+
         void load(int argc, char* argv[])
         {
-            po::options_description general{"General options"};
-            general.add_options()
-                ("help", "show help message")
-                ("version", "show program version");
+            auto options = get_command_line_options(argv[0]);
+            //auto config = get_config_options();
+            //auto common = get_common_options();
+
+            //options.add(common);
+            //config.add(common);
 
             po::variables_map vm;
-            po::store(po::parse_command_line(argc, argv, general), vm, true);
+
+            try
+            {
+                po::store(po::parse_command_line(argc, argv, options), vm, true);
+            }
+            catch (po::error& e)
+            {
+                std::clog << "Error: " << e.what() << '\n';
+                std::clog << "Error: Use '" << argv[0] << " --help' to get help about command line options\n";
+                std::exit(1);
+            }
+
+            po::notify(vm);
 
             if (vm.count("help"))
             {
-                std::cout << general << '\n';
+                std::cout << options << '\n';
                 std::exit(0);
             }
 
@@ -36,6 +67,19 @@ namespace rethinkmud
                 std::cout << "RethinkMUD version " << RETHINKMUD_VERSION << '\n';
                 std::exit(0);
             }
+
+            std::cout << "Configuration file = " << vm["config"].as<std::string>() << '\n';
+
+            //try
+            //{
+            //    po::store(po::parse_command_line(argc, argv, config), vm, true);
+            //}
+            //catch (po::error& e)
+            //{
+            //    std::clog << "Error: " << e.what() << '\n';
+            //    std::exit(1);
+            //}
+
         }
     }
 }
