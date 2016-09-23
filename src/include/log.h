22a3c201effebe2c49512510cd1f2c30f6d730c1
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-09-23 15:00:46 +0200

    Basic logging

--- src/include/log.h
+++ src/include/log.h
@@ -1,12 +1,50 @@
 #ifndef RETHINKMUD_LOG_H
 #define RETHINKMUD_LOG_H
 
+#include <sstream>
+#include <boost/log/trivial.hpp>
+#include <boost/log/sources/logger.hpp>
+
 namespace rethinkmud
 {
     namespace log
     {
         void init();
         void clean();
+
+        boost::log::sources::logger_mt& get_logger();
+
+        class logger
+        {
+        public:
+            logger(boost::log::trivial::severity_level const& severity)
+                : oss_{},
+                  severity_{severity}
+            {}
+
+            logger(logger&& l)
+                : oss_{std::move(l.oss_)},
+                  severity_{l.severity_}
+            {}
+
+            ~logger()
+            {
+                BOOST_LOG(get_logger()) << oss_.str();
+            }
+
+            template<typename T>
+            logger& operator<<(T const& x)
+            {
+                oss_ << x;
+                return *this;
+            }
+
+        private:
+            std::ostringstream oss_;
+            boost::log::trivial::severity_level const& severity_;
+        };
+
+        inline logger info() { return logger{boost::log::trivial::info}; }
     }
 }
 
--- src/log.cpp
+++ src/log.cpp
@@ -1,17 +1,33 @@
 #include "rethinkmud.h"
 
+#include <boost/log/trivial.hpp>
+
+namespace bl = boost::log;
+
 namespace rethinkmud
 {
     namespace log
     {
+        namespace
+        {
+            bl::sources::logger_mt logger_;
+        }
+
         void init()
         {
-
+            BOOST_LOG_TRIVIAL(info) << "Hello world from the logger";
+            info() << "Hello again" << ' ' << 1234 << " from logger";
+            BOOST_LOG(logger_) << "And again...";
         }
 
         void clean()
         {
 
+        }
+
+        bl::sources::logger_mt& get_logger()
+        {
+            return logger_;
         }
     }
 }
