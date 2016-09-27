8ba35c8db7ddaac236c1a834daea595795b640d9
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-09-27 14:33:24 +0200

    Give the server some character

---
+++ src/include/db/character.h
@@ -0,0 +1,17 @@
+#ifndef RETHINKMUD_CHARACTER_H
+#define RETHINKMUD_CHARACTER_H
+
+#include "thing.h"
+
+namespace rethinkmud
+{
+    namespace db
+    {
+        class character : public thing<character>
+        {
+
+        };
+    }
+}
+
+#endif //RETHINKMUD_CHARACTER_H
