91d10ce54f18ccca8859cd1813aa461536ca1582
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-09-27 11:18:09 +0200

    Started working on the MUD object classes

---
+++ src/include/db/location.h
@@ -0,0 +1,17 @@
+#ifndef RETHINKMUD_LOCATION_H
+#define RETHINKMUD_LOCATION_H
+
+#include "thing.h"
+
+namespace rethinkmud
+{
+    namespace db
+    {
+        class location : public thing
+        {
+
+        };
+    }
+}
+
+#endif //RETHINKMUD_LOCATION_H
---
+++ src/include/db/thing.h
@@ -0,0 +1,15 @@
+#ifndef RETHINKMUD_THING_H
+#define RETHINKMUD_THING_H
+
+namespace rethinkmud
+{
+    namespace db
+    {
+        class thing
+        {
+
+        };
+    }
+}
+
+#endif //RETHINKMUD_THING_H
